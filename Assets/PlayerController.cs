07bd74ec5bdf0c86052c88b8395b007c09e22256
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-06-12 22:45:48 -0500

    v0.01
    Core Game Screen, Player Control, Parallax Backgrounds.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Declare Rigidbody2D variable for storing child component
+	private Rigidbody2D bird;
+	// Vertical Velocity
+	private float velocity = 4;
+	// Use this for initialization
+	void Start () {
+		// Find Rigidbody2D component in child
+		bird = gameObject.GetComponentInChildren<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Give velocity in Y direction on pressing Space
+		if(Input.GetKeyDown(KeyCode.Space)){
+			bird.velocity = new Vector3(0,velocity,0);
+		}
+	}
+	
+	//TODO Write Collision Functions
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log ("Collision Occured");
+	}
+}
---
+++ Assets/Scripts/scroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	//Attach to 3D Quad with texture set to repeat
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2(Time.time*speed,0f);
+		renderer.material.mainTextureOffset = offset;
+	}
+}
